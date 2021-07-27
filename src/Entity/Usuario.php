21d8f7ccfd7abb0ada3ec023bb62a3d7b08fe223
luismamix <luisma.mix@gmail.com>
2021-07-27 19:08:11 +0200

    añadido el path /login para usuarios sin registrar en el fich security.yaml. Añadidas funciones getSalt() y eraseCredentials() referentes a encriptar la password al registrar el  usario, en el entity Usuario. Añadido PublicController, RegistrationController, Security Controller, formulario de registro de usuario

--- src/Entity/Usuario.php
+++ src/Entity/Usuario.php
@@ -4,11 +4,13 @@ namespace App\Entity;
 
 use App\Repository\UsuarioRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=UsuarioRepository::class)
+ * @UniqueEntity(fields={"email"}, message="There is already an account with this email")
  */
 class Usuario implements UserInterface, PasswordAuthenticatedUserInterface
 {
@@ -175,6 +177,7 @@ class Usuario implements UserInterface, PasswordAuthenticatedUserInterface
 
     public function getDireccion(): ?string
     {
+
         return $this->direccion;
     }
 
---
+++ src/Form/RegistrationFormType.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Usuario;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email')
+            ->add('agreeTerms', CheckboxType::class, [
+                'mapped' => false,
+                'constraints' => [
+                    new IsTrue([
+                        'message' => 'You should agree to our terms.',
+                    ]),
+                ],
+            ])
+            ->add('plainPassword', PasswordType::class, [
+                // instead of being set onto the object directly,
+                // this is read and encoded in the controller
+                'mapped' => false,
+                'attr' => ['autocomplete' => 'new-password'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter a password',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
+                    ]),
+                ],
+            ])
+            ->add('nombre', TextType::class,[
+                'label' => 'Nombre',
+                'required'   => true
+            ])
+            ->add('apellido', TextType::class,[
+                'label' => 'Apellido',
+                'required'   => true
+            ]) 
+            ->add('direccion', TextType::class,[
+                'label' => 'Direccion',
+                'required'   => true
+            ]) 
+            ->add('telefono', IntegerType::class,[
+                'label' => 'Telefono',
+                'required'   => true
+            ])  
+            ->add('roles',ChoiceType::class,[
+              /*   'label' => 'Rol',
+                'required'   => true, */
+                'choices'  => [
+                    'Admin' => '["ROLE_ADMIN"]',
+                    'Vendedor' =>  '["ROLE_VENDEDOR"]',
+                    'Comprador' => '["ROLE_COMPRADOR"]'
+                ]
+            ]) 
+            ->add('tipousuario', IntegerType::class,[
+                'label' => 'Tipo de usuario',
+                'required'   => true,
+                
+            ])
+
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Usuario::class,
+        ]);
+    }
+}
